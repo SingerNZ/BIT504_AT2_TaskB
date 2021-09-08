a4a6b5e1b6154136a98ecdf2f5c9bf37f331ba92
SingerNZ <karey.marie.nz@gmail.com>
2021-09-08 14:16:13 +1200

    Added more text. Should now read 'Hello World! How are you today? I hope lockdown is treating you well!'

--- Hello_World_TaskB/src/Hello_World_Example.java
+++ Hello_World_TaskB/src/Hello_World_Example.java
@@ -4,7 +4,7 @@ public class Hello_World_Example {
 	
 	public static void main(String[] args) {
 		// Simple 'Hello World' text to show in console
-		System.out.println("Hello World! How are you today?");
+		System.out.println("Hello World! How are you today? I hope lockdown is treating you well!");
 	} 
 
 }
