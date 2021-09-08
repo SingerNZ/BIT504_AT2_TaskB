4183d66556399d0fa8cb3135d0520e12b735b8ab
SingerNZ <karey.marie.nz@gmail.com>
2021-09-08 13:59:14 +1200

    Initial upload of project

---
+++ Hello_World_TaskB/src/Hello_World_Example.java
@@ -0,0 +1,10 @@
+ 
+public class Hello_World_Example {
+	
+	
+	public static void main(String[] args) {
+		// Simple 'Hello World' text to show in console
+		System.out.println("Hello World");
+	} 
+
+}
